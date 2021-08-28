7e5cc0f43cee8f158a02a9be24ae143aed1d53ab
AnubhavSrivastava <anubhavsrivastava93@gmail.com>
2021-08-28 09:55:36 +0530

    Exception type added

--- MilestoneProject2/utils/database_connection.py
+++ MilestoneProject2/utils/database_connection.py
@@ -2,14 +2,19 @@ import sqlite3
 
 
 class DatabaseConnection:
-    def __init__(self,host):
+    def __init__(self, host):
         self.connection = None
         self.host = host
 
     def __enter__(self):
-        self.connection = sqlite3.connect('host')      #data.db can be replaced with host so that we can use data.db in actual module for readability
+        self.connection = sqlite3.connect(
+            'host')  # data.db can be replaced with host so that we can use data.db in actual module for readability
         return self.connection
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.connection.commit()
-        self.connection.close()
+        if exc_type or exc_val or exc_tb:  # same as (if exc_type = not None or exc_val = not None  or exc_tb = not None ) :
+            print("book already exists in DB")
+            self.connection.close
+        else:
+            self.connection.commit()
+            self.connection.close()
